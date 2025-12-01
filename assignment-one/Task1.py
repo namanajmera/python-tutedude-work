11bd55ca29a18ff342d2799e8d2c92413530df29
Naman Ajmera <namanajmera@Namans-MacBook-Pro.local>
2025-12-01 21:15:26 +0530

    Added: Assignment One.

---
+++ assignment-one/Task1.py
@@ -0,0 +1,7 @@
+firstNumber = int(input("Enter the first number:- "))
+secondNumber = int(input("Enter the second number:- "))
+
+print("Addition: ", firstNumber + secondNumber)
+print("Subtraction: ", firstNumber - secondNumber)
+print("Multiplication: ", firstNumber * secondNumber)
+print("Division: ", firstNumber / secondNumber)
