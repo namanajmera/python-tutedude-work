50d6ab60a37b52d552a579e91f77ca9b0dabb250
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-12-06 21:49:23 +0530

    Added: Intro List.

---
+++ list/list_intro.py
@@ -0,0 +1,13 @@
+name = "Jhon"
+age = 20
+percent = 85.5
+
+student = ["Jhon", age, percent]
+print(student)
+
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print(days_of_week[0])
+print(days_of_week[1])
+print(len(days_of_week))
+print(days_of_week[len(days_of_week) - 1])
