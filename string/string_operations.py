2515016eba761616ace9c8f87f139cdcf3a4889d
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-12-06 21:36:03 +0530

    Added: String operations.

--- string/string_operations.py
+++ string/string_operations.py
@@ -11,3 +11,17 @@ print("z" in s1)
 # not in
 
 print("z" not in s1)
+
+# Counting string from a string
+
+s1 = "We are learning Python, And Python is awesome"
+s2 = "e"
+
+print(f"Occurrences of {s2} is {s1.count(s2)}")
+
+print(s1.capitalize())
+print(s1.upper())
+print(s1.lower())
+print(s1.swapcase())
+print(s1.title())
+print(s1.startswith("We"))
