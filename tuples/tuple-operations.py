55e1ff801ff1e01b721197936cf26c9931043c2c
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-12-07 16:36:02 +0530

    Added: Tuples are added.

---
+++ tuples/tuple-operations.py
@@ -0,0 +1,15 @@
+# Tuple
+
+t1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+t2 = (11, 12, 13, 14, 15, 16, 17, 18, 19,20)
+print(t1)
+print(t2)
+
+print(t1 + t2)
+print(t1[-1])
+# Give the error because tuple are immutables
+# t1[-1] = 20
+# print(t1)
+
+# Almost same operations we can perform in Tuples also
+# Like sum(), max(), concat() etc..
