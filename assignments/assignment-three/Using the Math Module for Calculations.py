8763fe19f677504e1caf1dff1f7fbd38589687b2
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-12-15 21:17:56 +0530

    Added: Using the Math Module.

---
+++ assignments/assignment-three/Using the Math Module for Calculations.py
@@ -0,0 +1,10 @@
+# Square root of the number
+# Natural logarithm (log base e) of the number
+# Sine of the number (in radians)
+
+import math
+
+val = int(input("Enter a number: "))
+print(f"Square root: {math.sqrt(val)}")
+print(f"Logarithm : {math.log(val)}")
+print(f"Sine: {math.sin(val)}")
