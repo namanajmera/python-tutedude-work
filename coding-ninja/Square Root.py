1bc3fa1fa15073d9dfa82bbb8792848e6c71f234
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-12-18 20:31:59 +0530

    Added: Square Root of a number.

---
+++ coding-ninja/Square Root.py
@@ -0,0 +1,8 @@
+# Problem statement
+# Given a number N, find its square root. You need to find and print only the integral part of square root of N.
+#
+# For eg. if number given is 18, answer is 4.
+import math
+value = int(input())
+
+print(int(math.sqrt(value)))
