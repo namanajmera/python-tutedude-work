c0cb985833c0a356881ffe1224491ffb364e1b0e
Naman Ajmera <cool.naman.ajmera@gmail.com>
2026-06-29 20:32:34 +0530

    Added: classes and objects.

---
+++ oops/creating_object_class.py
@@ -0,0 +1,18 @@
+
+class MyClass:
+    pass
+
+obj1 = MyClass()
+obj2 = MyClass()
+
+print(type(obj1))
+
+
+class Student:
+    """
+    This is a class Student to manage student information.
+    """
+    pass
+
+print(Student.__doc__)
+print(help(Student))
